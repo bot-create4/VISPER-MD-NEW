ea966159da37b73c0c333d4ed259c75446badc0c
Ransara Devnath <ransaramax87@gmail.com>
2025-07-06 09:15:32 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&SsZFBSrB#0BtEOoUUcClYUOc3VQpZj_QhdT90mTP92lrEUWyEw90' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&io4nRZCR#zJszHq4vmHnKVCdO-iS-6TyQ0HuAbsqXqDhVhcPYpU4' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
